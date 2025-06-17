5cf36210508da397fef5c9ca45e25e922f0d5fb0
saiful <si912999@gmail.com>
2025-06-17 23:37:12 +0600

    eee

--- app.js
+++ app.js
@@ -24,17 +24,16 @@ app.use(cookieParser());
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
 }).then(() => {
-  app.use("/api",router);
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('MongoDB connection error:', err);
 });
 
 
-
+app.use("/api",router);
 
 // Start server
 app.listen(PORT, () => {
