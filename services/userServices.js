0fe7297772afcc217cb051dd0138a50500caa2d7
Saiful Islam <132368544+SaifulET@users.noreply.github.com>
2025-06-18 03:13:15 +0600

    Update userServices.js

--- services/userServices.js
+++ services/userServices.js
@@ -42,7 +42,12 @@ export const login=async(req,res)=>{
 
         
         
-
+res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+            maxAge: 24 * 60 * 60 * 1000,
+        });
         res.status(200).json({
         message: 'Login successful',
         user: {
