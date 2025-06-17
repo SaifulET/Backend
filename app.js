1e0fd779933fd4c620a05e42dac5d2f1e3006105
saiful <si912999@gmail.com>
2025-06-18 00:34:34 +0600

    ee

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://guileless-dodol-f43ebb.netlify.app",  
-    credentials: true,             
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use("/api",router);
 
