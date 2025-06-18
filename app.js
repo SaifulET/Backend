daa065656ad7146211f85c89f58404d7b293b0ca
Saiful Islam <132368544+SaifulET@users.noreply.github.com>
2025-06-18 15:01:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://guileless-dodol-f43ebb.netlify.app", 
+    // origin: "https://guileless-dodol-f43ebb.netlify.app", 
+        origin:"http://localhost:5173",  
+
     credentials: true,             
   })
 );
