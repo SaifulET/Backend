444b49a9d67e0ef93516afc68962dab8f3a1fe3d
Saiful Islam <132368544+SaifulET@users.noreply.github.com>
2025-06-18 13:54:21 +0600

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const DataSchema= new mongoose.Schema({
    email:{type:String,unique:true,required:true,lowercase:true},
    password:{type:String,required:true},
-   name:{type:String, required:true},
-   profession:{type:String, required:true},
-   experience:{type:String, required:true},
-   institution:{type:String, required:true},
+   name:{type:String},
+   profession:{type:String},
+   experience:{type:String},
+   institution:{type:String},
 },{
     timestamps:true,
     versionKey:false
