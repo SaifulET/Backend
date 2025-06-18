276734d32d3cd9b0dde4035c1bbbda0da6e69990
Saiful Islam <132368544+SaifulET@users.noreply.github.com>
2025-06-18 13:21:45 +0600

    Update api.js

--- router/api.js
+++ router/api.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { login, ProfileUpdate, register, UserInfo } from '../services/userServices.js';
+import { Login, ProfileUpdate, Register, UserInfo } from '../services/userServices.js';
 import AuthMiddleware from '../Middleware/AuthMiddleware.js';
 
 const router = express.Router();
-router.post("/register",register)
-router.post("/login",login)
+router.post("/register",Register)
+router.post("/login",Login)
 router.get("/user",AuthMiddleware,UserInfo)
 router.post("/profile",AuthMiddleware,ProfileUpdate)
 
